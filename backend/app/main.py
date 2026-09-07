3546f29012a8b591f28e6b2c959b55eb17745eed
Maktab-PRO <adhamjon941770874@gmail.com>
2026-09-07 07:41:01 +0300

    Initialize database and update FastAPI app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from app.db import Base, engine
+from app.models.student import Student
 
 app = FastAPI(
     title="Axsikent IT API",
     description="Axsikent IT o'quv markazi uchun professional platforma",
     version="1.0.0"
 )
+
+
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
