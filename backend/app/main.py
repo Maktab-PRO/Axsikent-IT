f418ec2a5ae251871b44a52d1a1f539140429e33
Maktab-PRO <adhamjon941770874@gmail.com>
2026-09-07 08:18:28 +0300

    Add admin router to FastAPI application

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from app.db import Base, engine
+
 from app.models.student import Student
 from app.models.teacher import Teacher
+from app.models.admin import Admin
+
 from app.api.student import router as student_router
 from app.api.teacher import router as teacher_router
+from app.api.admin import router as admin_router
+
 
 app = FastAPI(
     title="Axsikent IT API",
@@ -11,10 +16,12 @@ app = FastAPI(
     version="1.0.0"
 )
 
+
 Base.metadata.create_all(bind=engine)
 
 app.include_router(student_router)
 app.include_router(teacher_router)
+app.include_router(admin_router)
 
 
 @app.get("/")
