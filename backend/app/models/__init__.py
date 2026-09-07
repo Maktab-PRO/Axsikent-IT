5ae68615e093a47c1fa3fcf2d774c0eb611dfcee
Maktab-PRO <adhamjon941770874@gmail.com>
2026-09-07 09:46:29 +0300

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,5 @@
 from app.models.student import Student
 from app.models.teacher import Teacher
 from app.models.admin import Admin
+from app.models.course import Course
+from app.models.category import Category
