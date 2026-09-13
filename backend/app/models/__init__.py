b1fd93c6993cf8f884e87d4f0a035a7385b046b5
Maktab-PRO <adhamjon941770874@gmail.com>
2026-09-13 18:45:07 +0500

    Import Lesson model in __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -20,3 +20,4 @@ from app.models.notification import Notification
 from app.models.course_module import CourseModule
 from app.models.lead import Lead
 from app.models.student_course import StudentCourse
+from app.models.lesson import Lesson
