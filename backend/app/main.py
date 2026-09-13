cacae3d8826fdc1db43ebcdc593289294cdd07cb
Maktab-PRO <adhamjon941770874@gmail.com>
2026-09-13 12:35:40 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,6 +14,7 @@ from app.api.course import router as course_router
 from app.api.admin_course import router as admin_course_router
 from app.api.group import router as group_router
 from app.api.lead import router as lead_router
+from app.api.student_course import router as student_course_router
 
 from app.seed import seed_data
 
@@ -62,6 +63,7 @@ app.include_router(course_router)
 app.include_router(admin_course_router)
 app.include_router(group_router)
 app.include_router(lead_router)
+app.include_router(student_course_router)
 
 # =========================
 
