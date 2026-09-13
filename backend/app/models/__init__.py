3e7e4519b9b6f5a4f597724a244db04a43b7e48d
Maktab-PRO <adhamjon941770874@gmail.com>
2026-09-13 12:25:41 +0300

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -19,3 +19,4 @@ from app.models.reward_rule import RewardRule
 from app.models.notification import Notification
 from app.models.course_module import CourseModule
 from app.models.lead import Lead
+from app.models.student_course import StudentCourse
