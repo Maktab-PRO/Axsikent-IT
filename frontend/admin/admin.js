53be2e8de6b17174f05b6e9f98f0d8402ed9ed24
Maktab-PRO <adhamjon941770874@gmail.com>
2026-09-17 23:52:01 +0500

    Update admin.js

--- frontend/admin/admin.js
+++ frontend/admin/admin.js
@@ -215,8 +215,9 @@ function openSection(section) {
     const sidebar = $("#sidebar");
 
     if (sidebar) {
-        sidebar.classList.remove("mobile-open");
-    }
+    sidebar.classList.remove("open");
+    sidebar.style.transform = "translateX(-100%)";
+}
 
     window.scrollTo({
         top: 0,
