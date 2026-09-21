8fd7dd87ef45c68bcc26c760eb14494647800597
Maktab-PRO <adhamjon941770874@gmail.com>
2026-09-21 14:51:29 +0300

    Register online exam models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -31,3 +31,5 @@ from app.models.training import Training, TrainingRegistration
 from app.models.exam import Exam, ExamRegistration
 
 from app.models.ai_telegram_submission import AITelegramSubmission
+
+from app.models.online_exam import OnlineExam, OnlineExamQuestion, OnlineExamAttempt
